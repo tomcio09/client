d74b0ff2f9647200e17e38f0f1f652b5c6d2d3c5
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-05-22 10:10:38 +0200

    Create HudModule.java

---
+++ src/main/java/pl/goated/client/module/impl/HudModule.java
@@ -0,0 +1,32 @@
+package pl.goated.client.module.impl;
+
+import pl.goated.client.module.Module;
+import pl.goated.client.module.settings.BooleanSetting;
+import pl.goated.client.module.settings.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HudModule extends Module {
+	private final List<Setting<?>> settings = new ArrayList<>();
+	
+	public final BooleanSetting showWatermark;
+	public final BooleanSetting showArrayList;
+	public final BooleanSetting showCoordinates;
+	
+	public HudModule() {
+		super("HUD", "Heads Up Display customization", Category.RENDER);
+		
+		showWatermark = new BooleanSetting("Show Watermark", true);
+		showArrayList = new BooleanSetting("Show ArrayList", true);
+		showCoordinates = new BooleanSetting("Show Coordinates", true);
+		
+		settings.add(showWatermark);
+		settings.add(showArrayList);
+		settings.add(showCoordinates);
+	}
+	
+	public List<Setting<?>> getSettings() {
+		return settings;
+	}
+}
