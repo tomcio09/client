4fbd9b66bb6c755a4f0c7a609a8a24dbc355ca22
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-05-22 10:10:26 +0200

    Create GuiModule.java

---
+++ src/main/java/pl/goated/client/module/impl/GuiModule.java
@@ -0,0 +1,38 @@
+package pl.goated.client.module.impl;
+
+import pl.goated.client.module.Module;
+import pl.goated.client.module.settings.BooleanSetting;
+import pl.goated.client.module.settings.ColorSetting;
+import pl.goated.client.module.settings.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuiModule extends Module {
+	private final List<Setting<?>> settings = new ArrayList<>();
+	
+	public final ColorSetting borderColor;
+	public final ColorSetting backgroundColor;
+	public final ColorSetting textColor;
+	public final BooleanSetting blur;
+	
+	public GuiModule() {
+		super("GUI", "Customize GUI appearance", Category.RENDER);
+		
+		borderColor = new ColorSetting("Border Color", 0xFF000000);
+		backgroundColor = new ColorSetting("Background Color", 0xFFFFFFFF);
+		textColor = new ColorSetting("Text Color", 0xFF000000);
+		blur = new BooleanSetting("Background Blur", true);
+		
+		settings.add(borderColor);
+		settings.add(backgroundColor);
+		settings.add(textColor);
+		settings.add(blur);
+		
+		setEnabled(true); // GUI module always enabled
+	}
+	
+	public List<Setting<?>> getSettings() {
+		return settings;
+	}
+}
