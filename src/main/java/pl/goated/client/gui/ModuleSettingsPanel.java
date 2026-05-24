a0f45efd7921d84f3d8372f71410e91a965e58be
Tomasz Gregor <127117177+tomcio09@users.noreply.github.com>
2026-05-24 10:17:23 +0200

    Update ModuleSettingsPanel.java

--- src/main/java/pl/goated/client/gui/ModuleSettingsPanel.java
+++ src/main/java/pl/goated/client/gui/ModuleSettingsPanel.java
@@ -8,6 +8,7 @@ import pl.goated.client.module.Module;
 import pl.goated.client.module.impl.GuiModule;
 import pl.goated.client.module.settings.BooleanSetting;
 import pl.goated.client.module.settings.ColorSetting;
+import pl.goated.client.module.settings.FloatSetting;
 import pl.goated.client.module.settings.Setting;
 import pl.goated.client.util.RenderUtil;
 
@@ -43,7 +44,6 @@ public class ModuleSettingsPanel {
 	private void initSettings() {
 		try {
 			List<Setting<?>> settings = getModuleSettings();
-			
 			int yOffset = 50;
 			for (Setting<?> setting : settings) {
 				if (setting instanceof ColorSetting colorSetting) {
@@ -52,6 +52,9 @@ public class ModuleSettingsPanel {
 				} else if (setting instanceof BooleanSetting booleanSetting) {
 					components.add(new BooleanSettingComponent(booleanSetting, PADDING, yOffset, width - PADDING * 2));
 					yOffset += SETTING_HEIGHT;
+				} else if (setting instanceof FloatSetting floatSetting) {
+					components.add(new FloatSettingComponent(floatSetting, PADDING, yOffset, width - PADDING * 2));
+					yOffset += SETTING_HEIGHT + 10;
 				}
 			}
 		} catch (Exception e) {
@@ -73,16 +76,15 @@ public class ModuleSettingsPanel {
 	
 	public void render(DrawContext context, int mouseX, int mouseY, float delta) {
 		GuiModule guiModule = (GuiModule) GoatedClient.getInstance().getModuleManager().getModuleByName("GUI");
-		
 		int bgColor = guiModule.backgroundColor.getValue();
 		int borderColor = guiModule.borderColor.getValue();
 		int textColor = guiModule.textColor.getValue();
 		
 		RenderUtil.drawRoundedRect(context, x, y, width, height, 10, bgColor);
 		RenderUtil.drawRoundedRectOutline(context, x, y, width, height, 10, 2, borderColor);
-		
 		RenderUtil.drawRect(context, x, y, width, 40, RenderUtil.adjustAlpha(borderColor, 50));
-		context.drawText(MinecraftClient.getInstance().textRenderer, 
+		
+		context.drawText(MinecraftClient.getInstance().textRenderer,
 			module.getName() + " Settings", x + PADDING, y + 15, textColor, false);
 		
 		int closeX = x + width - CLOSE_BUTTON_SIZE - 10;
@@ -90,7 +92,7 @@ public class ModuleSettingsPanel {
 		boolean closeHovered = mouseX >= closeX && mouseX <= closeX + CLOSE_BUTTON_SIZE &&
 			mouseY >= closeY && mouseY <= closeY + CLOSE_BUTTON_SIZE;
 		
-		RenderUtil.drawRoundedRect(context, closeX, closeY, CLOSE_BUTTON_SIZE, CLOSE_BUTTON_SIZE, 4, 
+		RenderUtil.drawRoundedRect(context, closeX, closeY, CLOSE_BUTTON_SIZE, CLOSE_BUTTON_SIZE, 4,
 			closeHovered ? 0xFFFF0000 : RenderUtil.adjustAlpha(borderColor, 100));
 		context.drawText(MinecraftClient.getInstance().textRenderer, "X", closeX + 6, closeY + 6, 0xFFFFFFFF, false);
 		
@@ -105,12 +107,8 @@ public class ModuleSettingsPanel {
 	
 	public boolean mouseClicked(double mouseX, double mouseY, int button) {
 		if (activeColorPicker != null) {
-			if (activeColorPicker.mouseClicked(mouseX, mouseY, button)) {
-				return true;
-			} else {
-				activeColorPicker = null;
-				return false;
-			}
+			if (activeColorPicker.mouseClicked(mouseX, mouseY, button)) return true;
+			else { activeColorPicker = null; return false; }
 		}
 		
 		int closeX = x + width - CLOSE_BUTTON_SIZE - 10;
@@ -123,21 +121,23 @@ public class ModuleSettingsPanel {
 		}
 		
 		for (SettingComponent component : components) {
-			if (component.mouseClicked(x, y, mouseX, mouseY, button)) {
-				if (component instanceof ColorSettingComponent colorComp) {
-					activeColorPicker = new ColorPicker(colorComp.setting, (int) mouseX, (int) mouseY);
-				}
+			SettingComponent.ClickResult result = component.mouseClicked(x, y, mouseX, mouseY, button);
+			if (result == SettingComponent.ClickResult.OPEN_COLOR_PICKER && component instanceof ColorSettingComponent colorComp) {
+				activeColorPicker = new ColorPicker(colorComp.setting, (int) mouseX, (int) mouseY);
+				return true;
+			} else if (result == SettingComponent.ClickResult.HANDLED) {
 				return true;
 			}
 		}
-		
 		return false;
 	}
-
-	// DODANE: Rozwiązuje problem z suwakiem!
+	
 	public void mouseReleased(double mouseX, double mouseY, int button) {
 		if (activeColorPicker != null) {
 			activeColorPicker.mouseReleased();
+		}
+		for (SettingComponent component : components) {
+			component.mouseReleased(x, y, mouseX, mouseY);
 		}
 	}
 	
@@ -151,8 +151,85 @@ public class ModuleSettingsPanel {
 	}
 	
 	private interface SettingComponent {
+		enum ClickResult { NONE, HANDLED, OPEN_COLOR_PICKER }
 		void render(DrawContext context, int panelX, int panelY, int mouseX, int mouseY, GuiModule guiModule);
-		boolean mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button);
+		ClickResult mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button);
+		default void mouseReleased(int panelX, int panelY, double mouseX, double mouseY) {}
+	}
+	
+	private static class FloatSettingComponent implements SettingComponent {
+		private final FloatSetting setting;
+		private final int x, y, width;
+		private boolean dragging = false;
+		
+		public FloatSettingComponent(FloatSetting setting, int x, int y, int width) {
+			this.setting = setting;
+			this.x = x;
+			this.y = y;
+			this.width = width;
+		}
+		
+		@Override
+		public void render(DrawContext context, int panelX, int panelY, int mouseX, int mouseY, GuiModule guiModule) {
+			int absoluteX = panelX + x;
+			int absoluteY = panelY + y;
+			
+			// Nazwa i wartość
+			context.drawText(MinecraftClient.getInstance().textRenderer,
+				setting.getName(), absoluteX, absoluteY + 5, guiModule.textColor.getValue(), false);
+			context.drawText(MinecraftClient.getInstance().textRenderer,
+				setting.getDisplayValue(), absoluteX + width - 50, absoluteY + 5,
+				guiModule.textColor.getValue(), false);
+			
+			// Pasek suwaka
+			int sliderY = absoluteY + 20;
+			int sliderHeight = 10;
+			
+			// Tło suwaka
+			RenderUtil.drawRoundedRect(context, absoluteX, sliderY, width, sliderHeight, 5,
+				RenderUtil.adjustAlpha(guiModule.borderColor.getValue(), 80));
+			
+			// Wypełnienie suwaka
+			int fillWidth = (int) (width * setting.getPercent());
+			if (fillWidth > 0) {
+				RenderUtil.drawRoundedRect(context, absoluteX, sliderY, fillWidth, sliderHeight, 5, 0xFF00CC00);
+			}
+			
+			// Kursor suwaka
+			int cursorX = absoluteX + fillWidth - 5;
+			RenderUtil.drawRoundedRect(context, cursorX, sliderY - 3, 10, sliderHeight + 6, 5, 0xFFFFFFFF);
+			
+			// Aktualizacja gdy przeciągamy
+			if (dragging) {
+				float percent = (float)(mouseX - absoluteX) / width;
+				setting.setFromPercent(percent);
+				GoatedClient.getInstance().getConfigManager().save();
+			}
+		}
+		
+		@Override
+		public ClickResult mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button) {
+			int absoluteX = panelX + x;
+			int absoluteY = panelY + y;
+			int sliderY = absoluteY + 20;
+			
+			if (mouseX >= absoluteX && mouseX <= absoluteX + width &&
+				mouseY >= sliderY - 3 && mouseY <= sliderY + 13) {
+				dragging = true;
+				float percent = (float)(mouseX - absoluteX) / width;
+				setting.setFromPercent(percent);
+				return ClickResult.HANDLED;
+			}
+			return ClickResult.NONE;
+		}
+		
+		@Override
+		public void mouseReleased(int panelX, int panelY, double mouseX, double mouseY) {
+			if (dragging) {
+				dragging = false;
+				GoatedClient.getInstance().getConfigManager().save();
+			}
+		}
 	}
 	
 	private static class BooleanSettingComponent implements SettingComponent {
@@ -171,13 +248,12 @@ public class ModuleSettingsPanel {
 			int absoluteX = panelX + x;
 			int absoluteY = panelY + y;
 			
-			context.drawText(MinecraftClient.getInstance().textRenderer, 
+			context.drawText(MinecraftClient.getInstance().textRenderer,
 				setting.getName(), absoluteX, absoluteY + 5, guiModule.textColor.getValue(), false);
 			
 			int toggleX = absoluteX + width - 45;
 			int toggleWidth = 40;
 			int toggleHeight = 20;
-			
 			boolean hovered = mouseX >= toggleX && mouseX <= toggleX + toggleWidth &&
 				mouseY >= absoluteY && mouseY <= absoluteY + toggleHeight;
 			
@@ -185,13 +261,12 @@ public class ModuleSettingsPanel {
 			if (hovered) bgColor = RenderUtil.adjustBrightness(bgColor, 20);
 			
 			RenderUtil.drawRoundedRect(context, toggleX, absoluteY, toggleWidth, toggleHeight, 10, bgColor);
-			
 			int circleX = setting.getValue() ? toggleX + toggleWidth - 18 : toggleX + 2;
 			RenderUtil.drawRoundedRect(context, circleX, absoluteY + 2, 16, 16, 8, 0xFFFFFFFF);
 		}
 		
 		@Override
-		public boolean mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button) {
+		public ClickResult mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button) {
 			int absoluteX = panelX + x;
 			int absoluteY = panelY + y;
 			int toggleX = absoluteX + width - 45;
@@ -200,9 +275,9 @@ public class ModuleSettingsPanel {
 				mouseY >= absoluteY && mouseY <= absoluteY + 20) {
 				setting.toggle();
 				GoatedClient.getInstance().getConfigManager().save();
-				return true;
+				return ClickResult.HANDLED;
 			}
-			return false;
+			return ClickResult.NONE;
 		}
 	}
 	
@@ -222,30 +297,29 @@ public class ModuleSettingsPanel {
 			int absoluteX = panelX + x;
 			int absoluteY = panelY + y;
 			
-			context.drawText(MinecraftClient.getInstance().textRenderer, 
+			context.drawText(MinecraftClient.getInstance().textRenderer,
 				setting.getName(), absoluteX, absoluteY + 5, guiModule.textColor.getValue(), false);
 			
 			int previewX = absoluteX + width - 80;
-			
 			RenderUtil.drawRoundedRect(context, previewX, absoluteY, 70, 25, 5, setting.getValue());
 			RenderUtil.drawRoundedRectOutline(context, previewX, absoluteY, 70, 25, 5, 1, guiModule.borderColor.getValue());
 			
 			String hex = String.format("#%08X", setting.getValue());
-			context.drawText(MinecraftClient.getInstance().textRenderer, 
+			context.drawText(MinecraftClient.getInstance().textRenderer,
 				hex, previewX + 5, absoluteY + 8, RenderUtil.getContrastColor(setting.getValue()), false);
 		}
 		
 		@Override
-		public boolean mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button) {
+		public ClickResult mouseClicked(int panelX, int panelY, double mouseX, double mouseY, int button) {
 			int absoluteX = panelX + x;
 			int absoluteY = panelY + y;
 			int previewX = absoluteX + width - 80;
 			
 			if (mouseX >= previewX && mouseX <= previewX + 70 &&
 				mouseY >= absoluteY && mouseY <= absoluteY + 25) {
-				return true;
+				return ClickResult.OPEN_COLOR_PICKER;
 			}
-			return false;
+			return ClickResult.NONE;
 		}
 	}
 }
